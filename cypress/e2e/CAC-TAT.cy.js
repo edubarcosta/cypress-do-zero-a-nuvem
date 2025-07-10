99f719f30e6388c7edcd733c9848db970518bd01
Eduardo Barros <eduardibarros@gmail.com>
2025-07-09 23:35:04 -0300

    adicionando comentários

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,6 +1,10 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
   it('verifica o título da aplicação', () => {
     cy.visit('./src/index.html')
+    // no .vist() o caminho é relativo ao arquivo cypress.config.js
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    //.title() é um comando do Cypress que verifica o título da página
+    //.should('be.equal', 'informação esperada')
+    // not.be.equal é a negativa. should() = deve.
   })
 })
